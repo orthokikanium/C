12179151afa5f89782fb00da0f1cee804a6c3b14
2023年的小菜鸡_________ <91087648+orthokikanium@users.noreply.github.com>
2023-01-16 19:51:25 +0800

    Update 2023-01-16-P1209.cpp

--- 2023-01-16-P1209.cpp
+++ 2023-01-16-P1209.cpp
@@ -14,9 +14,6 @@ bool cmp(int a,int b) {
 	}
 }
 
-
-
-
 int main() {
 	int m, s, c;
 	cin >> m >> s >> c;
@@ -26,7 +23,6 @@ int main() {
 	for (int i = 0; i < c; i++) {
 		cin >> tmp;
 		used.push_back(tmp);
-		
 	}
 	int length;
 	if (m >= c) {
@@ -41,34 +37,14 @@ int main() {
 				dot.push_back(used[i] - used[i - 1] - 1);
 			}
 		}
-		//for (int i = 0; i < used.size(); i++) {
-		//	cout << used[i] << " ";
-		//}
-		//cout << endl;
-
-		//for (int i = 0; i < dot.size(); i++) {
-		//	cout<<dot[i]<<" ";
-		//}
-		//cout << endl;
 
 		sort(dot.begin(), dot.end(), cmp);
 		int sum = 0;
 		for (int i = 0; i < m - 1; i++) {
 			sum += dot[i];
 		}
-
 		length = used[used.size() - 1] - used[0] + 1 - sum;
 	}
-	
 	cout << length;
-
-
 	return 0;
-
-
-
-
-
-
-
 }
